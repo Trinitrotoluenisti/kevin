2b0288643a462f96451232c8a3f3b064cb471d7b
TommyAnd <Tommaso.Andreanelli04@gmail.com>
2020-02-01 12:25:17 +0100

    Prove #1

--- website/application/routes.py
+++ website/application/routes.py
@@ -4,7 +4,4 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    with open('application/templates/content1.html') as f:
-        content = f.read()
-
-    return render_template('layout.html', content=content)
+    return render_template('layout.html')
