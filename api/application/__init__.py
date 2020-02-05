da15c0d8fb3ce23c2d38a5a57e30c80bc95ef147
gianluparri03 <gianlucaparri03@gmail.com>
2020-02-05 13:49:07 +0100

    a little db

--- api/application/__init__.py
+++ api/application/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 
+
+# app initializing
 app = Flask(__name__)
+
+# app configs
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# api initializing
 api = Api(app)
 
+
+from .database import *
 from .routes import *
--- api/application/routes.py
+++ api/application/routes.py
@@ -3,14 +3,18 @@ from . import api
 from flask_restful import Resource, reqparse
 
 
+""" RESOURCES """
 class Login(Resource):
     def post(self):
+    	# Parse args
         parser = reqparse.RequestParser()
         parser.add_argument('username', type=str, location='form', required=True)
         parser.add_argument('password', type=str, location='form', required=True)
         args = parser.parse_args()
 
+        # return loged-in, no matter what
         return 'logged-in!'
 
 
+""" ADDING RESOURCES TO API """
 api.add_resource(Login, '/login')
