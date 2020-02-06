b19253fe1a9df228b9e3dbf7c1aaed5c318aecbb
TommyAnd <Tommaso.Andreanelli04@gmail.com>
2020-02-06 21:27:15 +0100

    aggiunti bug front

--- website/application/routes.py
+++ website/application/routes.py
@@ -4,8 +4,5 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('layout.html')
+    return render_template('home.html')
 
-@app.route('/articolo')
-def articolo():
-    return render_template('articolo.html')
