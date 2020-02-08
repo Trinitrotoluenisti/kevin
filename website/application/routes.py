b2b1075e47a201a84f58f432ea1204809e8d528e
gianluparri03 <gianlucaparri03@gmail.com>
2020-02-08 15:16:55 +0100

    register is now compatible with website

--- website/application/routes.py
+++ website/application/routes.py
@@ -2,59 +2,74 @@ from . import app, server
 from flask import render_template, request, jsonify
 from requests import get, post
 
+
 #pagine standard
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    # Return the template if it's a GET request
     if request.method == "GET":
         return render_template('login.html')
+
+    # If it's POST...
     else:
+        # fetch data
         username = request.form["user"]
         password = request.form["pass"]
-        r = post(server+"/login", data={"username": username, "password": password})
-        if r.status_code==200: 
+
+        # make a requets to the apis
+        r = post(server + "/login", data={"username": username, "password": password})
+
+        # if it's ok, return the register
+        if r.status_code == 200: 
             return render_template('/login.html', token=r.json()["token"])
         else: 
-            return render_template('/home.html', alert=r.json()["message"])
+            return render_template('/home.html', alert=r.json()["msg"])
 
-@app.route('/signup', methods=["GET", "POST"])
+
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    # Return the template if it's a GET request
     if request.method == "GET":
         return render_template('register.html')
+
+    # If it's POST...
     else:
+        # fetch data
         username = request.form["user"]
+        email = request.form["mail"]
         password = request.form["pass"]
-        r = post(server+"/login", data={"username": username, "password": password})
-        if r.status_code==200: 
+
+        # make a requets to the apis
+        r = post(server + "/signup", data={"username": username, "password": password, 'email': email})
+
+        # if it's ok, return the register
+        if r.status_code == 200: 
             return render_template('/register.html', token=r.json()["token"])
         else: 
-            return render_template('/home.html', alert=r.json()["message"])
+            return render_template('/home.html', alert=r.json()["msg"])
+
 
 #pagine utenti loggati
-
 @app.route('/settings')
 def settings():
     return "Settings"
 
 #pagine admin
-
 @app.route('/admin')
 def admin():
     return "da fare solo per gli amministratori"
 
 #errori
-
 @app.errorhandler(404)
 def page_not_found(e):
     return "wewaglio se so fregati la pagina", 404
 
 #roba tommy
-
 @app.route('/tom')
 def tommy():
     return render_template('geekia.html')
-
-
--- website/application/static/js/register.js
+++ website/application/static/js/register.js
@@ -1,18 +1,9 @@
 function testpass(modulo){
-    // Verifico che il campo password sia valorizzato in caso contrario
-    // avverto dell'errore tramite un Alert
-    if (modulo.pass.value == ""){
-        alert("Errore: inserire una password!")
-        modulo.pass.focus()
-        return false
-    }
-    // Verifico che le due password siano uguali, in caso contrario avverto
-    // dell'errore con un Alert
     if (modulo.pass.value != modulo.passconfirm.value) {
-        alert("La password inserita non coincide con la prima!")
+        alert("Password aren't matching!")
         modulo.pass.focus()
         modulo.pass.select()
         return false
     }
     return true
-    }
+}
