4f1d0bc38a80bd7e77e9c2a426c82286e9010902
gianluparri03 <gianlucaparri03@gmail.com>
2020-02-11 17:59:07 +0100

    dropdowns working!

--- website/application/__init__.py
+++ website/application/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from requests import get, ConnectionError
 
-app = Flask(__name__) 
+
+app = Flask(__name__)
 server = "http://localhost:8080"
 
-from .routes import *
+# Check if there's a server online
+try:
+	get(server)
+except ConnectionError:
+	raise ConnectionError("There isn't an API running in that URL") from None
 
+
+from .routes import *
